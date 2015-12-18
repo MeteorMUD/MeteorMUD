6f9c82c11413e14ae476d0b52bcb7c8e38076ada
Nathan Douglas <ndouglas@devontechnologies.com>
2015-12-18 09:58:14 -0600

    Adds scaffolding-yell command.
    See #18.

--- packages/commands-scaffolding/server/commands-scaffolding.js
+++ packages/commands-scaffolding/server/commands-scaffolding.js
@@ -1,5 +1,8 @@
 // Write your package code here!
 
+// The scaffolding command.
+ScaffoldingCommand = {};
+
 // Sets the command name.
 ScaffoldingCommand.name = "scaffolding";
 
@@ -63,13 +66,80 @@ ScaffoldingCommand.handler = function (arguments, completionHandler) {
   });
 };
 
+// The "yell" scaffolding command.
+YellCommand = {};
+
+// Sets the command name.
+YellCommand.name = "scaffolding-yell";
+
+// Sets the command summary.
+YellCommand.summary = ScaffoldingCommand.summary;
+
+// Sets the command text.
+YellCommand.text = ScaffoldingCommand.text;
+
+// Sets the command categories.
+YellCommand.categories = ScaffoldingCommand.categories;
+
+// Sets the command search terms.
+YellCommand.searchTerms = ScaffoldingCommand.searchTerms;
+
+// Sets the command permsissions.
+YellCommand.permissions = ScaffoldingCommand.permissions;
+
+// Sets the command type.
+YellCommand.type = ScaffoldingCommand.type;
+
+/**
+ * Handle the command.
+ *
+ * @param {[string]} arguments - The arguments supplied to the command.
+ * @param {function} completionHandler - A completion handler that takes an
+ * object.
+ */
+
+YellCommand.handler = function (arguments, completionHandler) {
+
+  // Fail catastrophically if we're passed an arguments object that isn't an array.
+  if (!MeteorMUD.Underscore.isArray(arguments)) {
+    throw new Error("Arguments is not an array.");
+  }
+
+  // Catch the case where we don't have any subcommands.
+  if (!subcommands || subcommands.length === 0) {
+    return MeteorMUD.complete(completionHandler, {
+      success: false,
+      error: {
+        message: "Could not execute '" + ChatChannelCommand.name + "' command.",
+        reason: "Chat-channel is not fully implemented; wait until there are subcommands!",
+      },
+    });
+  }
+
+  // Otherwise, list the valid subcommands.
+  // Should shift this to some functionality of Commands.
+  return MeteorMUD.complete(completionHandler, {
+    success: true,
+    message: "Yelled '" + arguments.join(" ") + "',",
+  });
+};
+
 Meteor.startup(function () {
   // Startup code goes here.
 
   // Clean the command.
   Schemas.Command.clean(ScaffoldingCommand);
 
-  // Add the help command.
+  // Add the scaffolding command.
   MeteorMUD.Commands.addCommand(ScaffoldingCommand);
 
+  // Clean the command.
+  Schemas.Command.clean(YellCommand);
+
+  // Add the yell command.
+  MeteorMUD.Commands.addCommand(YellCommand);
+
+  // Add the yell subcommand.
+  MeteorMUD.Commands.addSubcommand("scaffolding", "yell");
 });
+
